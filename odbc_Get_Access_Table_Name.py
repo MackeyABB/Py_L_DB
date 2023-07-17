5a6685c90adde61ae273bc86eca1d59bd07aec55
MackeyLao <Mackey.lao@gmail.com>
2023-07-17 17:04:22 +0800

    feat: 测试不同的connStr是否可以用。

--- odbc_Get_Access_Table_Name.py
+++ odbc_Get_Access_Table_Name.py
@@ -5,7 +5,13 @@ https://stackoverflow.com/questions/12704305/return-column-names-from-pyodbc-exe
 import pypyodbc
 
 # 64 bit Access DB with ODBC
-connStr = "DSN=CIS_PartLib_P_64;Uid=cadence_port;Pwd=Cadence_CIS.3;"
+# connStr = "DSN=CIS_PartLib_P_64;Uid=cadence_port;Pwd=Cadence_CIS.3;"
+# connStr = "DSN=CIS_PartLib_P_64_access;"
+connStr = "DSN=AD_Sqlite3;"
+
+
+print("connStr is:\t{}\n".format(connStr))
+
 # 32 bit Access DB with ODBC, can not with in pypyodbc.
 # pypyodbc.Error: ('IM014', '[IM014] [Microsoft][ODBC Driver Manager] The specified DSN contains an architecture mismatch between the Driver and Application')
 # connStr = "DSN=CIS_PartLib_P_32;Uid=cadence_port;Pwd=Cadence_CIS.3;"
@@ -21,7 +27,7 @@ cursor.columns(table='01-Capacitors')
 columns = [column[0] for column in cursor.description]
 print(columns)
 
-
+print("\nMethos2:\n")
 # 尝试先执行SQL命令后，再获取column名称，可行
 sql = "SELECT * FROM [01-Capacitors];"
 cursor.execute(sql)
